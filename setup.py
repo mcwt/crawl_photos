4576fdcc457cdb693c5cab83099985771d6190b8
machongwei <machongwei_vendor@sensetime.com>
2020-04-22 20:49:20 +0800

    init

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+import crawl_photos
+
+
+setup(
+    name='crawl_photos',
+    version=crawl_photos.__version__,
+    author=crawl_photos.__author__,
+    author_email='1830mm@sina.com',
+    description='a test',
+    url='https://github.com/',
+
+)
